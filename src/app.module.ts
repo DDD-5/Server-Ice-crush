aa4049adf655fb7732fbdd7fa859ff98395ad4f4
유진님 <suddenlydisappear200@gmail.com>
2021-04-20 00:25:24 +0900

    시퀄라이즈 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { config } from './config';
 
 @Module({
-  imports: [],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'mysql',
+      host: config.db_host,
+      port: 3306,
+      username: config.db_user_name,
+      password: config.db_password,
+      database: config.db_name,
+      models: [],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/config.ts
@@ -0,0 +1,7 @@
+export const config: any = {
+  db_host: process.env.DB_HOST,
+  db_port: process.env.PORT,
+  db_user_name: process.env.DB_USER_NAME,
+  db_password: process.env.DB_PASSWORD,
+  db_name: process.env.DB_NAME,
+};
